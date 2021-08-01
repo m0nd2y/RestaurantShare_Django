71d42b9050062c3735f33e80067b1b149d8776db
m0nd2y <ldj6192@gmail.com>
2021-08-01 19:16:38 +0900

    'commit'

--- RestaurantShare/ShareRes/views.py
+++ RestaurantShare/ShareRes/views.py
@@ -3,13 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request) :
-    return HttpResponse("inedx")
+    #return HttpResponse("inedx")
+    return render(request, 'ShareRes/index.html')
 
 def restaurantDetail(request) :
-    return HttpResponse("restaurantDetail")
+    #return HttpResponse("restaurantDetail")
+    return render(request, 'ShareRes/restaurantDetail.html')
     
 def restaurantCreate(request) :
-    return HttpResponse("restaurantCreate")
+    #return HttpResponse("restaurantCreate")
+    return render(request, 'ShareRes/restaurantCreate.html')
     
 def categoryCreate(request) :
-    return HttpResponse("categoryCreate")
+    #return HttpResponse("categoryCreate")
+    return render(request, 'ShareRes/categoryCreate.html')
