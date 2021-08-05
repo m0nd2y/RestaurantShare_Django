e12ab7cd9e9c3552ef6e7ec88a84d41c18be779d
m0nd2y <ldj6192@gmail.com>
2021-08-05 20:22:21 +0900

    'commit'

--- RestaurantShare/ShareRes/urls.py
+++ RestaurantShare/ShareRes/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('restaurantDetail/<str:res_id>', views.restaurantDetail, name='resDetailPage'),
+    path('restaurantDetail/updatePage/<str:res_id>',views.restaurantUpdate, name='resUpdatePage')
     path('restaurantCreate/', views.restaurantCreate, name='resCreatePage'),
     path('restaurantCreate/create', views.Create_restaurant, name='resCreate'),
     path('categoryCreate/', views.categoryCreate, name='cateCreatePage'),
