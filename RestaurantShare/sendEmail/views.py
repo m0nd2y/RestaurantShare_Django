e030da9c60b44ce7344df5ff80408dce7a2ec229
m0nd2y <ldj6192@gmail.com>
2021-08-06 13:42:30 +0900

    'commit'

--- RestaurantShare/sendEmail/views.py
+++ RestaurantShare/sendEmail/views.py
@@ -1,7 +1,14 @@
 from django.http.response import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
 
 # Create your views here.
 def sendEmail(requests) :
+    checked_res_list = requests.POST.getlist('checks')
+    inputReceiver = requests.POST['inputReceiver']
+    inputTitle = requests.POST['inputTitle']
+    inputContent = request.POST['inputContent']
+    print(checked_res_list,"/",inputReceiver,"/",inputTitle,"/",inputContent) 
     return HttpResponseRedirect(reverse('index'))
     #return HttpResponse("sendEmail")
