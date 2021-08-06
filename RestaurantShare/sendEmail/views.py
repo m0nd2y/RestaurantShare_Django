1dac6b19593a758005e32cf47d17f3dbdfb3d9c7
m0nd2y <ldj6192@gmail.com>
2021-08-06 13:39:09 +0900

    'commit'

--- RestaurantShare/sendEmail/views.py
+++ RestaurantShare/sendEmail/views.py
@@ -1,6 +1,7 @@
-from django.http.response import Http404, HttpResponse
+from django.http.response import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 def sendEmail(requests) :
-    return HttpResponse("sendEmail")
+    return HttpResponseRedirect(reverse('index'))
+    #return HttpResponse("sendEmail")
