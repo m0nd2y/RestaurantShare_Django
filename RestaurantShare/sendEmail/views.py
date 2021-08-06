43954d58ff689fe6f1b274281d2d6a3f0ec70b7a
m0nd2y <ldj6192@gmail.com>
2021-08-06 13:57:21 +0900

    'commit'

--- RestaurantShare/sendEmail/views.py
+++ RestaurantShare/sendEmail/views.py
@@ -2,7 +2,9 @@ from django.http.response import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from ShareRes.models import *
-
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 # Create your views here.
 def sendEmail(requests) :
